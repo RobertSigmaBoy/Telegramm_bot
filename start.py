9fd9018f017f79b6a71e7547e43956151db803e2
Robert sigma <r.mughdusian@gmail.com>
2025-03-09 17:11:14 +0300

    добавил Dp.py

--- start.py
+++ start.py
@@ -29,7 +29,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     else:
         await context.bot.send_message(
             chat_id=update.effective_chat.id,
-            text=f"Привет,{update.effective_user.first_name}!Давай поиграем в камень ножницы бумагу , ты первый, кстати я о тебе многое знаю твое ади:{update.effective_user.id}твое имя:{update.effective_user.first_name}твоя иконка:{update.effective_user.get_profile_photos}твой язык:{update.effective_user.language_code}  твое последнее имя:{update.effective_user.last_name}  ну так что ходишь?",
+            text=f"Привет,{update.effective_user.first_name}!Давай поиграем в игры кстати я о тебе многое знаю твое ади:{update.effective_user.id}твое имя:{update.effective_user.first_name}твоя иконка:{update.effective_user.get_profile_photos}твой язык:{update.effective_user.language_code}  твое последнее имя:{update.effective_user.last_name}  ну так что ходишь?",
             reply_markup=markup,
         )
         message = await context.bot.send_message(
